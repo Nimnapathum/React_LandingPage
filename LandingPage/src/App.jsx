1a96eee7e578014f7bd85af05810284820388126
Nimnapathum <pathumeshan123@gmail.com>
2024-07-13 00:00:50 +0530

    idk what to commit

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,19 +1,22 @@
 import Navbar from './components/navbar/Navbar'
-// import { Route , Routes } from 'react-router-dom'
-// import Home from './pages/Home'
-// import About from './pages/About'
-// import ContactUs from './pages/ContactUs'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import ContactUs from './pages/ContactUs'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar />
-      {/* <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/About' element={<About />}/>
-        <Route path='/ContactUs' element={<ContactUs/>}/>
-      </Routes> */}
-    </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contactus' element={<ContactUs />} />
+        </Routes>
+      </BrowserRouter>
+      
+     </div>
   )
 }
 
